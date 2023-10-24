f16339d487797c70690c6b2e2c02dba71717d5f0
Bui Quoc Khanh <quckhnh1412@gmail.com>
2023-10-24 16:10:40 +0700

    add login and sign up

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // Import Express framework
-const express = require('express');
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 
 // Set the view engine to EJS
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "handlebars", // Specify the extension for handlebars files
+  helpers: {},
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
 // Set the views directory to the "views" folder
 
 // Set up a basic route to render an EJS template
-app.get('/', (req, res) => {
-  res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("login");
 });
-
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 // Start the server on port 3000
 const port = 3000;
 app.listen(port, () => {
